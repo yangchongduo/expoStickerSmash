697367d1a32b77602a9f0d4b6212ee21d99ab323
yangchongduo <yangchongduo@shoplineapp.com>
2025-06-01 14:07:26 +0800

    feat: ing

---
+++ app.config.js
@@ -0,0 +1,42 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return 'com.yourname.stickersmash.dev';
+  }
+
+  if (IS_PREVIEW) {
+    return 'com.yourname.stickersmash.preview';
+  }
+
+  return 'com.yourname.stickersmash';
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return 'StickerSmash (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'StickerSmash (Preview)';
+  }
+
+  return 'StickerSmash: Emoji Stickers';
+};
+
+
+export default ({ config }) => ({
+  ...config,
+  name: getAppName(),
+  ios: {
+    // buildNumber: 1,
+    ...config.ios,
+    bundleIdentifier: getUniqueIdentifier(),
+  },
+  android: {
+    // versionCode: 1,
+    ...config.android,
+    package: getUniqueIdentifier(),
+  },
+});
