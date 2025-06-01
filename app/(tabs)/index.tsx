6ca1642baf9d198746dc5ee37eb45c6d876a248c
yangchongduo <yangchongduo@shoplineapp.com>
2025-06-01 16:11:09 +0800

    feat: ing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -148,7 +148,7 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>index screen</Text>
+      <Text style={styles.text}>index232323 screen</Text>
     </View>
   );
 }
